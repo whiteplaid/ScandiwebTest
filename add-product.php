12e40afc57be3a10c7a10fc036f79edfdf3d2f41
whiteplaid <whitemailru@gmail.com>
2022-03-31 19:09:38 +0300

    update

--- add-product.php
+++ add-product.php
@@ -1,15 +1,18 @@
 <link rel="stylesheet" href="css\style.css">
-<title>Product Add</title>
-<nav class="d-flex card-header">
-  <h3 class="my-0 ml4">Product Add</h5>
-  <div class="d-flex sl mt1 mr4">
-    <a class="button-del p3" href="/">CANCEL</a>
-  </div>
-</nav>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 <script src="js/check.js"></script>
 <script src="js/t.js"></script>
+<title>Product Add</title>
+<nav class="d-flex card-header">
+  <h3 class="my-0 ml4">Product Add</h5>
+</nav>
 <form id="product_form" action="/i">
+<table>
+        <tr class = 'p34'>
+        <td class="b"><button type='submit'  class='button' id='save-product-btn' name='Save' value='Save' onclick='t()'>SAVE</button></td>
+        <td class="b"><button type='button' class='button' name="CANCEL" onclick="window.location.href='/'">CANCEL</button></td>
+        </tr>
+    </table>
     <table class='card mt5 ml4'>
         <tr><td>SKU</td> <td><input type='text' name='sku' id='sku' placeholder='type sku id'></td></tr>
         <tr><td></td> <td><p class="error" id="esku"></p></td></tr>
@@ -33,8 +36,7 @@
     </table>
 
     <div id='dynamic'></div>
-  
-<button type='submit'  class='button p34' id='save-product-btn' name='Save' value='Save' onclick='t()'>SAVE</button>
+
 </form>
 
 
