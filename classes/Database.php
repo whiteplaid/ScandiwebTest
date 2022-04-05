4708455491205ed52ed32deb5afd2f666da5e12b
whiteplaid <whiteplaid@users.noreply.github.com>
2022-04-05 19:54:44 +0300

    Add files via upload

---
+++ classes/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Whiteplaid;
+
+use \mysqli;
+class Database {
+    private $host = 'localhost';
+    private $db = 'product_list';
+    private $user = 'root';
+    private $pass = '';
+    public static $conn;
+
+    public function __construct() {
+        self::$conn = new mysqli($this->host,$this->user,$this->pass,$this->db);
+    }
+
+    public function getConnection () {  
+        return self::$conn;
+    }
+
+    public function readSku ($sku) {
+        $query = "SELECT sku FROM product WHERE sku='$sku'";
+        $result = self::$conn->query($query)->fetch_object();
+        $r = ($result == null)? '' : $result->sku;
+        return print_r($r);
+    }
+
+}
+?>
